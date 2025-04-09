604852b7b3a08aa4e2afe825440833640e5c78f5
Sheene Prestin <sheeneprestin72@gmail.com>
2025-04-09 14:52:08 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,16 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Personal_Budget_Tracker_with_Expense_Analytics
+namespace PersonalBudgetTracker
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
+   public enum TransactionType
+{
+    Income,
+    Expense
+}
+ public class Transaction
+ {
+     public string Description { get; set; }
+     public decimal Amount { get; set; }
+     public TransactionType Type { get; set; }
+     public string Category { get; set; }
+     public DateTime Date { get; set; }
+     public Guid Id { get; private set; }
+public Transaction(string description, decimal amount, TransactionType type, string category, DateTime date)
+{
+    if (string.IsNullOrWhiteSpace(description))
+        throw new ArgumentException("Description cannot be empty", nameof(description));
 
-        }
+    if (amount <= 0)
+        throw new ArgumentException("Amount must be greater than zero", nameof(amount));
+
+    if (string.IsNullOrWhiteSpace(category))
+        throw new ArgumentException("Category cannot be empty", nameof(category));
+
+    Description = description;
+    Amount = amount;
+    Type = type;
+    Category = category;
+    Date = date;
+    Id = Guid.NewGuid();
+}
+         public Transaction()
+    {
+        Id = Guid.NewGuid();
+    }
+
+    public override string ToString()
+    {
+        string typeSymbol = Type == TransactionType.Income ? "+" : "-";
+        return $"{Date.ToShortDateString()} | {typeSymbol}{Amount:C} | {Category} | {Description}";
     }
 }
+     public class BudgetTracker
+ {
+     public List<Transaction> Transactions { get; private set; }
+
+     public BudgetTracker()
+     {
+         Transactions = new List<Transaction>();
+     }
