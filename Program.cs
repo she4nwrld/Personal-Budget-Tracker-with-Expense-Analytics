e7e5acaa7c4e4711c5646410fbc672b5644ce20a
sheen <sheen@SHEENE>
2025-04-09 12:40:06 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personal_Budget_Tracker_with_Expense_Analytics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
